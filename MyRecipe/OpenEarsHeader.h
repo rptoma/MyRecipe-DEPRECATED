f20b12d0d6f4c7f29165254789b318eb17cf11eb
Alexandru11 <allexandru.radu11@gmail.com>
2017-04-24 12:26:21 +0300

    Testing the functionality on mobile device.

---
+++ MyRecipe/OpenEarsHeader.h
@@ -0,0 +1,20 @@
+//
+//  OpenEarsHeader.h
+//  MyRecipe
+//
+//  Created by Eduard Radu on 24/04/2017.
+//  Copyright © 2017 Toma Radu-Petrescu. All rights reserved.
+//
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */
--- MyRecipe/RecipeStepViewController.swift
+++ MyRecipe/RecipeStepViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 import AVFoundation
 
-class RecipeStepViewController: UIViewController {
+class RecipeStepViewController: UIViewController, OEEventsObserverDelegate {
     
     @IBOutlet weak var taskLabel: UILabel!
     @IBOutlet weak var taskDescriptionView: UITextView!
     @IBOutlet weak var nextButtonOutlet: UIButton!
     
+    var openEarsEventsObserver = OEEventsObserver()
     let speechSynthesizer = AVSpeechSynthesizer()
     
     var taskCounter:Int = 0
@@ -23,9 +24,23 @@ class RecipeStepViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.openEarsEventsObserver.delegate = self
+        
         nextButtonOutlet.layer.cornerRadius = 10
         
         updateTask()
+        
+        AVAudioSession.sharedInstance().requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                self.createVoiceRecognition()
+                // Microphone allowed, do what you like!
+                
+            } else {
+                // User denied microphone. Tell them off!
+                
+            }
+        }
         
     }
     
@@ -62,6 +77,38 @@ class RecipeStepViewController: UIViewController {
         else {
             finishMessageAlert()
         }
+    }
+    
+    func pocketsphinxDidReceiveHypothesis(_ hypothesis: String!, recognitionScore: String!, utteranceID: String!) { // Something was heard
+        print("Local callback: The received hypothesis is \(hypothesis!) with a score of \(recognitionScore!) and an ID of \(utteranceID!)" + "Asta-i tinta!")
+        
+    }
+    
+    func createVoiceRecognition(){
+        let lmGenerator = OELanguageModelGenerator()
+        let words = ["next", "next step", "next task", "task"] // These can be lowercase, uppercase, or mixed-case.
+        let name = "NameIWantForMyLanguageModelFiles"
+        let err: Error! = lmGenerator.generateLanguageModel(from: words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"))
+        
+        if(err != nil) {
+            print("Error while creating initial language model: \(err)")
+        } else {
+            let lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name) // Convenience method to reference the path of a language model known to have been created successfully.
+            let dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionary(withRequestedName: name) // Convenience method to reference the path of a dictionary known to have been created successfully.
+            enableVoiceRecognition(path1: lmPath!, path2: dicPath!)
+            
+        }
+    }
+    
+    func enableVoiceRecognition(path1:String, path2:String){
+        // OELogging.startOpenEarsLogging() //Uncomment to receive full OpenEars logging in case of any unexpected results.
+        do {
+            try OEPocketsphinxController.sharedInstance().setActive(true) // Setting the shared OEPocketsphinxController active is necessary before any of its properties are accessed.
+        } catch {
+            print("Error: it wasn't possible to set the shared instance to active: \"\(error)\"")
+        }
+        
+        OEPocketsphinxController.sharedInstance().startListeningWithLanguageModel(atPath: path1, dictionaryAtPath: path2, acousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"), languageModelIsJSGF: false)
     }
     
     func finishMessageAlert(){
