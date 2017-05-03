79b22b4686dff90a3cccadfa64fc569a64528d13
rptoma <rptoma@gmail.com>
2017-05-03 12:18:21 +0300

    added name in recipe description

--- MyRecipe/RecipeDescription.swift
+++ MyRecipe/RecipeDescription.swift
@@ -9,24 +9,27 @@
 import Foundation
 
 class RecipeDescription: CustomStringConvertible {
+    public var name: String?
     public var images = [String]()
     public var duration: Int?
     public var recipeDescription: String?
     public var ingredients = [Ingredient]()
     
     public var description: String {
+        let name = self.name ?? "No name"
         let images = self.images.description
         let duration = self.duration ?? -1
         let recipeDescription = self.recipeDescription ?? "no description"
         let ingredients = self.ingredients.description
         
-        var ret = images + "\n" + String(duration) + "\n"
-        ret = ret + recipeDescription + "\n" + ingredients
+        var ret = name + "\n" + images + "\n" + String(duration)
+        ret = "\n" + ret + recipeDescription + "\n" + ingredients
         
         return ret
     }
     
     public init(fromJson dictionary: [String: AnyObject]) {
+        name = dictionary["name"] as? String
         duration = dictionary["duration"] as? Int
         if let imagesArray = dictionary["images"] as? [String] {
             images = imagesArray
