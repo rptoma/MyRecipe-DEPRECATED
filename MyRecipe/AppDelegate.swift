a7f40619515c9869785fad75cbb5a31041e9aaaa
rptoma <rptoma@gmail.com>
2017-05-08 20:38:58 +0300

    No text shadow is needed now. The image is dimmed instead.

--- MyRecipe/AppDelegate.swift
+++ MyRecipe/AppDelegate.swift
@@ -17,6 +17,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        for family: String in UIFont.familyNames
+//        {
+//            print("\(family)")
+//            for names: String in UIFont.fontNames(forFamilyName: family)
+//            {
+//                print("== \(names)")
+//            }
+//        }
+//        
+//        if let font = UIFont(name: "GillSans", size: 20) {
+//            UINavigationBar.appearance().titleTextAttributes = [NSFontAttributeName: font]
+//        }
+        
         return true
     }
 
--- MyRecipe/RecipeTableViewCell.swift
+++ MyRecipe/RecipeTableViewCell.swift
@@ -20,33 +20,35 @@ class RecipeTableViewCell: UITableViewCell {
     
     var uid: String!
     
+    func addTextShadow() {
+        nameLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        nameLabel.layer.shadowOpacity = 0.5
+        nameLabel.layer.shadowRadius = 3
+        nameLabel.layer.shouldRasterize = true
+        nameLabel.layer.rasterizationScale = UIScreen.main.scale
+
+        difficultyLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        difficultyLabel.layer.shadowOpacity = 0.5
+        difficultyLabel.layer.shadowRadius = 3
+        difficultyLabel.layer.shouldRasterize = true
+        difficultyLabel.layer.rasterizationScale = UIScreen.main.scale
+
+        durationLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        durationLabel.layer.shadowOpacity = 0.5
+        durationLabel.layer.shadowRadius = 3
+        durationLabel.layer.shouldRasterize = true
+        durationLabel.layer.rasterizationScale = UIScreen.main.scale
+
+        delimiterLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
+        delimiterLabel.layer.shadowOpacity = 0.5
+        delimiterLabel.layer.shadowRadius = 3
+        delimiterLabel.layer.shouldRasterize = true
+        delimiterLabel.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        nameLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
-        nameLabel.layer.shadowOpacity = 0.5
-        nameLabel.layer.shadowRadius = 1.5
-        nameLabel.layer.shouldRasterize = true
-        nameLabel.layer.rasterizationScale = UIScreen.main.scale
-        
-        difficultyLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
-        difficultyLabel.layer.shadowOpacity = 0.5
-        difficultyLabel.layer.shadowRadius = 1.5
-        difficultyLabel.layer.shouldRasterize = true
-        difficultyLabel.layer.rasterizationScale = UIScreen.main.scale
-        
-        durationLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
-        durationLabel.layer.shadowOpacity = 0.5
-        durationLabel.layer.shadowRadius = 1.5
-        durationLabel.layer.shouldRasterize = true
-        durationLabel.layer.rasterizationScale = UIScreen.main.scale
-        
-        delimiterLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
-        delimiterLabel.layer.shadowOpacity = 0.5
-        delimiterLabel.layer.shadowRadius = 1.5
-        delimiterLabel.layer.shouldRasterize = true
-        delimiterLabel.layer.rasterizationScale = UIScreen.main.scale
-        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -58,13 +60,13 @@ class RecipeTableViewCell: UITableViewCell {
         if let imgUrl = recipe.icon_image {
             if let url = URL(string: imgUrl) {
                 let resource = ImageResource(downloadURL: url, cacheKey: nil)
-                let processor = BlurImageProcessor(blurRadius: 4.1)
+                let processor = BlurImageProcessor(blurRadius: 3) >> ColorControlsProcessor(brightness: 0, contrast: 1, saturation: 1, inputEV: -0.3)
                 backgroundImageView.kf.indicatorType = .activity
                 backgroundImageView.kf.setImage(with: resource, placeholder: nil, options: [.processor(processor)], progressBlock: nil, completionHandler: nil)
             }
         }
         else {
-            backgroundImageView.image = UIImage(named: Base.DEFAULT_IMAGE_PATH)?.kf.blurred(withRadius: CGFloat(4.1))
+            backgroundImageView.image = UIImage(named: Base.DEFAULT_IMAGE_PATH)?.kf.blurred(withRadius: CGFloat(3)).kf.adjusted(brightness: 0, contrast: 1, saturation: 1, inputEV: -0.3)
         }
     }
     
