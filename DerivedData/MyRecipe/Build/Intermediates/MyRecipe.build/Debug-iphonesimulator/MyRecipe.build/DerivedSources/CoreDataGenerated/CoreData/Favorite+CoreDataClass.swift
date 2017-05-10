0fbe88103b7173e7b38cf7880a6149ec0768ca49
Alexandru11 <allexandru.radu11@gmail.com>
2017-05-10 10:00:52 +0300

    update

---
+++ DerivedData/MyRecipe/Build/Intermediates/MyRecipe.build/Debug-iphonesimulator/MyRecipe.build/DerivedSources/CoreDataGenerated/CoreData/Favorite+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Favorite+CoreDataClass.swift
+//  
+//
+//  Created by Eduard Radu on 10/05/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Favorite)
+public class Favorite: NSManagedObject {
+
+}
---
+++ DerivedData/MyRecipe/Build/Intermediates/MyRecipe.build/Debug-iphonesimulator/MyRecipe.build/DerivedSources/CoreDataGenerated/CoreData/Favorite+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Favorite+CoreDataProperties.swift
+//  
+//
+//  Created by Eduard Radu on 10/05/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+extension Favorite {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Favorite> {
+        return NSFetchRequest<Favorite>(entityName: "Favorite")
+    }
+
+    @NSManaged public var uid: String?
+
+}
