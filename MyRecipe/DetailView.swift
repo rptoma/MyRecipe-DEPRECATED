a94129e9a3e3c8448a738c36a6d78786707ccc99
Alexandru11 <allexandru.radu11@gmail.com>
2017-05-25 18:37:07 +0300

    changed text view in label for description

--- MyRecipe/DetailView.swift
+++ MyRecipe/DetailView.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 class DetailView: UIView {
-    @IBOutlet weak var descriptionLabel: UILabel!
+  
     
-    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var descriptionTextView: UILabel!
+
+    @IBOutlet weak var descriptionLabel: UILabel!
   
     @IBOutlet weak var durationLabel: UILabel!
     
